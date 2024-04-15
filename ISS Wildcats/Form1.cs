714ff888f52a5eb869b83f94ddd7263c7ca73d3f
LeonteDenis <denis.leonte123@gmail.com>
2024-04-15 23:38:15 +0300

    First iteration

--- ISS Wildcats/Form1.cs
+++ ISS Wildcats/Form1.cs
@@ -6,5 +6,45 @@ namespace ISS_Wildcats
         {
             InitializeComponent();
         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
